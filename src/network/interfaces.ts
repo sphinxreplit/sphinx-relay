4e8f433bebe7c0f26d31d20486a1381a4a60e1f5
EvanFeenstra1 <evanfeenstra@gmail.com>
2022-02-22 10:24:42 -0800

    fix a bot photo error, more explicit Msg interface

--- src/network/interfaces.ts
+++ src/network/interfaces.ts
@@ -1,4 +1,4 @@
-interface MessageContent {
+export interface MessageContent {
   uuid: string
   content: string
   amount: number
@@ -11,27 +11,33 @@ interface MessageContent {
   originalMuid?: string
   status?: number
   purchaser?: number
+  invoice?: string
+}
+
+export interface ChatContent {
+  uuid: string
+  type?: number
+  members?: { [k: string]: any }
+  name?: string
+  groupKey?: string
+  host?: string
+}
+
+export interface SenderContent {
+  id?: number
+  pub_key: string
+  alias: string
+  role: number
+  route_hint?: string
+  photo_url?: string
+  contact_key?: string
 }
 
 export interface Msg {
   type: number
   message: MessageContent
-  chat: {
-    uuid: string
-    type?: number
-    members?: { [k: string]: any }
-    name?: string
-    groupKey?: string
-    host?: string
-  }
-  sender: {
-    id?: number
-    pub_key: string
-    alias: string
-    role: number
-    route_hint?: string
-    photo_url?: string
-  }
+  chat: ChatContent
+  sender: SenderContent
   bot_id?: any
   bot_name?: string
   recipient_id?: any
