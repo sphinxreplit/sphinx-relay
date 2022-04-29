98d2f5e22a11cb8bcbf2c4bcbe9a6860c2394182
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-04-29 14:04:33 -0500

    added JSON strigify so we can actually read the object

--- dist/src/utils/socket.js
+++ dist/src/utils/socket.js
@@ -113,7 +113,7 @@ const send = (body, tenant) => {
 };
 exports.send = send;
 const sendJson = (object, tenant) => {
-    logger_1.sphinxLogger.info(`Socket sendJson ${object}`);
+    logger_1.sphinxLogger.info(`Socket sendJson ${JSON.stringify(object)}`);
     (0, exports.send)(JSON.stringify(object), tenant);
 };
 exports.sendJson = sendJson;
