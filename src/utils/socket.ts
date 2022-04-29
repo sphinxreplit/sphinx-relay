e82848a7ab299585d5add8930671ca4231cf9d24
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-04-29 13:38:18 -0500

    added log into socket

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -111,5 +111,6 @@ export const send = (body, tenant) => {
 }
 
 export const sendJson = (object, tenant: number) => {
+  sphinxLogger.warning(`Socket sendJson ${object}`)
   send(JSON.stringify(object), tenant)
 }
