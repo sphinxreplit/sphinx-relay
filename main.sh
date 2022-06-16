3e6df92b05113fe1c8bf2369ca7084396307f538
sphinxreplit <sphinxreplit@gmail.com>
2022-06-16 20:27:35 +0000

    new change

--- main.sh
+++ main.sh
@@ -1,1 +1,9 @@
-rm package-lock.json || npm install && npm run build && git checkout -b $(date +'%m/%d/%Y-%H.%M.%S') && git add . && git commit -m "new change" && git push && node ./dist/app.js --config=./config/app.json --db=./config/config.json
+export BRANCHNAME=$(date +'%m/%d/%Y-%H.%M.%S') && 
+rm package-lock.json || 
+npm install && 
+npm run build && 
+git checkout -b $BRANCHNAME && 
+git add . && 
+git commit -m "new change" && 
+git push --set-upstream sphinxreplit $BRANCHNAME &&
+node ./dist/app.js --config=./config/app.json --db=./config/config.json
