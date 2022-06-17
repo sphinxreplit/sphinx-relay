5ea22aa4c43fe7f3a7cc4c5b5291062379dd2372
sphinxreplit <sphinxreplit@gmail.com>
2022-06-17 20:39:33 +0000

    new change

--- main.sh
+++ main.sh
@@ -10,5 +10,6 @@ echo "ENTER THIS USERNAME: sphinxreplit" &&
 echo "Enter this github access token as password: $github_access_token" &&
 echo "NOTE THAT YOU WILL NEED TO WAIT 5-7 MINUTES TO SEE CHANGES REFLECTED ON EC2 instance" &&
 git push --set-upstream sphinxreplit $BRANCHNAME &&
+echo "I'm going to make you wait 2 minutes to update the ec2 instance, please be patient" &&
 sleep 120 &&
 node ./dist/app.js --config=./config/app.json --db=./config/config.json
