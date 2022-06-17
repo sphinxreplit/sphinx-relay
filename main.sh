33aee4d92d3460dbe6bbcddfc9e8ba488507aff6
sphinxreplit <sphinxreplit@gmail.com>
2022-06-17 14:24:25 +0000

    new change

--- main.sh
+++ main.sh
@@ -6,5 +6,5 @@ git checkout -b $BRANCHNAME &&
 git add . && 
 git commit -m "new change" && 
 echo "$github_access_token" &&
-echo | git push --set-upstream sphinxreplit $BRANCHNAME &&
+sleep 30 | echo | git push --set-upstream sphinxreplit $BRANCHNAME &&
 node ./dist/app.js --config=./config/app.json --db=./config/config.json
