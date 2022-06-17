dc37732a60fdbd94be23c60e20227bc3cb09e69a
sphinxreplit <sphinxreplit@gmail.com>
2022-06-17 20:34:07 +0000

    new change

--- main.sh
+++ main.sh
@@ -5,7 +5,8 @@ npm run build &&
 git checkout -b $BRANCHNAME &&
 git add . && 
 git commit -m "new change" || 
-echo "ENTER THIS USERNAME: sphinxreplit \n Enter this github access token as password: $github_access_token \n YOU WILL NEED TO WAIT AROUND 5-7 MINUTES TILL CHANGES ARE AVAILIBLE ON EC2 RELAY NODES" &&
+echo "ENTER THIS USERNAME: sphinxreplit" &&
+echo "Enter this github access token as password: $github_access_token" &&
+echo "NOTE THAT YOU WILL NEED TO WAIT 5-7 MINUTES TO SEE CHANGES REFLECTED ON EC2 instance" &&
 git push --set-upstream sphinxreplit $BRANCHNAME &&
-sleep 120 &&
 node ./dist/app.js --config=./config/app.json --db=./config/config.json
